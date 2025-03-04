e23cee3299cf05b386a55c5320adf868570f757b
mbowman21 <sstraberry101@gmail.com>
2025-03-04 10:56:17 -0600

    made create value type function

--- ValueType/dao.py
+++ ValueType/dao.py
@@ -4,3 +4,11 @@ from db_helper import ConnectionManager
 @ConnectionManager.requires_connection
 def get_value_type_by_id(id: str):
     return ValueType.objects.with_id(id)
+
+@ConnectionManager.requires_connection
+def create_value_type(name: str) -> str:
+    allowed_values = []
+
+    value_type = ValueType(name=name, allowed_values=allowed_values)
+    value_type.save()
+    return value_type.id.binary.hex()
