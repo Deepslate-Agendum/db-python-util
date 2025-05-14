2bcdae61579ae0dd466bc2ab676a2d263dca81c9
creallfluharty <creallfluharty@gmail.com>
2025-05-14 12:29:12 -0500

    add get enum method and move create from db-init

--- db_helper.py
+++ db_helper.py
@@ -50,3 +50,33 @@ def get_tag(name) -> Optional[AllowedValue]:
     tag_value_type = ValueType.objects(name="Tag").first()
     tag = AllowedValue.objects(value=name, value_type=tag_value_type).first()
     return tag
+
+@ConnectionManager.requires_connection
+def create_enum(enum_name, value_names):
+    value_type = ValueType(
+        name=enum_name,
+        allowed_values=[],
+    )
+    value_type.save()
+
+    values_map = {}
+    for value_name in value_names:
+        value = AllowedValue(
+            value=value_name,
+            value_type=value_type,
+        )
+        value.save()
+        values_map[value_name] = value
+
+    value_type.update(allowed_values=values_map.values())
+    value_type.save()
+
+    return value_type, values_map
+
+@ConnectionManager.requires_connection
+def get_enum(name):
+    value_type = ValueType.objects(name=name).first()
+    values = AllowedValue.objects(value_type=value_type)
+    value_map = {value.value: value for value in values}
+
+    return value_type, value_map
